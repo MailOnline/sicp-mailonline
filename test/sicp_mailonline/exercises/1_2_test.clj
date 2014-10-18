d25d6db4d683a3be532e412fa79aa6ca0d7c15f1
Nigel Perkins <nigelhp@yahoo.co.uk>
2014-10-18 21:34:14 +0100

    Exercise 1.2

---
+++ test/sicp_mailonline/exercises/1_2_test.clj
@@ -0,0 +1,21 @@
+;; Exercise 1.2
+;; Translate the following expression into prefix form
+;;
+;; 5 + 4 + (2 - (3 - (6 + 4/5)))
+;; -----------------------------
+;;        3(6 - 2)(2 - 7)
+
+(ns sicp-mailonline.exercises.1-2-test
+  (:require [clojure.test :refer :all]))
+
+(deftest exercise1-2
+  (is (= -37/150
+         (/ (+ 5 
+               4 
+               (- 2 
+                  (- 3 
+                     (+ 6 
+                        (/ 4 5)))))
+            (* 3 
+               (- 6 2) 
+               (- 2 7))))))
