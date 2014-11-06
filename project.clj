680477fa208b15328b25b2efbc5ee0a2f3f2c38c
reborg <reborg@reborg.net>
2014-11-06 13:03:52 +0000

    added midje plumbing

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :plugins [[com.jakemccrary/lein-test-refresh "0.5.2"]])
+  :repl-options {:init (do
+                         (require 'midje.repl)
+                         (midje.repl/autotest :filter (complement :slow)))}
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.1"]
+                             [com.jakemccrary/lein-test-refresh "0.5.2"]]}})
