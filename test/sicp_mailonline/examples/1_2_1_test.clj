25bc3e186684c9835ae0ca558c5977544a1c92cf
Nigel Perkins <nigelhp@yahoo.co.uk>
2014-11-16 22:18:29 +0000

    Removed code duplication in tests for examples

--- test/sicp_mailonline/examples/1_2_1_test.clj
+++ test/sicp_mailonline/examples/1_2_1_test.clj
@@ -4,17 +4,16 @@
   (:require [sicp-mailonline.examples.1-2-1 :refer :all]
             [clojure.test :refer :all]))
 
+(defn- test-factorial [f]
+  (are [n x] (= x (f n))
+       1   1
+       6 720))
+
 (deftest example1-2-1
   (testing "linear recursive factorial"
     (let [factorial rfactorial]
-      (is (= 1
-             (factorial 1)))
-      (is (= 720
-             (factorial 6)))))
+      (test-factorial factorial)))
 
   (testing "linear iterative factorial"
     (let [factorial ifactorial]
-      (is (= 1
-             (factorial 1)))
-      (is (= 720
-             (factorial 6))))))
+      (test-factorial factorial))))
--- test/sicp_mailonline/examples/1_2_2_test.clj
+++ test/sicp_mailonline/examples/1_2_2_test.clj
@@ -4,32 +4,26 @@
   (:require [sicp-mailonline.examples.1-2-2 :refer :all]
             [clojure.test :refer :all]))
 
+(defn- test-fibonacci [f]
+  (are [n x] (= x (f n))
+       0 0
+       1 1
+       2 1
+       3 2
+       4 3
+       5 5
+       6 8
+       7 13
+       8 21))
+
 (deftest example1-2-2
   (testing "tree recursive Fibonacci"
     (let [fib rfib]
-      (are [n x] (= x (fib n))
-           0 0
-           1 1
-           2 1
-           3 2
-           4 3
-           5 5
-           6 8
-           7 13
-           8 21)))
+      (test-fibonacci fib)))
 
   (testing "linear iterative Fibonacci"
     (let [fib ifib]
-      (are [n x] (= x (fib n))
-           0 0
-           1 1
-           2 1
-           3 2
-           4 3
-           5 5
-           6 8
-           7 13
-           8 21)))
+      (test-fibonacci fib)))
 
   (testing "counting change"
     (is (= 292
