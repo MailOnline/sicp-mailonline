13db34e387cab861f7a7c15c9717cdcd38781b3e
Nigel Perkins <nigelhp@yahoo.co.uk>
2014-11-29 17:56:03 +0000

    Use zero based index

--- test/sicp_mailonline/exercises/1_19_test.clj
+++ test/sicp_mailonline/exercises/1_19_test.clj
@@ -7,16 +7,17 @@
 (deftest exercise1.19
   (testing "fast Fibonacci"
     (are [n x] (= x (fib n))
-          1  0
+          0  0
+          1  1
           2  1
-          3  1
-          4  2
-          5  3
-          6  5
-          7  8
-          8 13
-          9 21
-         10 34)))
+          3  2
+          4  3
+          5  5
+          6  8
+          7 13
+          8 21
+          9 34
+         10 55)))
 
 ;;  n steps (invocations of fib-iter)
 ;;  1  2
