2ecbb0114a473d7b575a1821d6dbed040fd2c921
reborg <reborg@reborg.net>
2014-12-02 18:07:47 +0000

    Coin change basic

--- src/sicp_mailonline/paragraphs/1_2_2_countchange.clj
+++ src/sicp_mailonline/paragraphs/1_2_2_countchange.clj
@@ -1,2 +1,20 @@
 (ns sicp-mailonline.paragraphs.1-2-2-countchange)
 
+(defn first-denomination [kinds-of-coins]
+  (cond (= kinds-of-coins 1) 1
+        (= kinds-of-coins 2) 5
+        (= kinds-of-coins 3) 10
+        (= kinds-of-coins 4) 25
+        (= kinds-of-coins 5) 50))
+
+(defn cc [amount kinds-of-coins]
+  (cond (= amount 0) 1
+        (or (< amount 0) (= kinds-of-coins 0)) 0
+        :else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins))))
+
+(defn count-change [amount]
+  (cc amount 5))
--- test/sicp_mailonline/paragraphs/1_2_2_countchange_test.clj
+++ test/sicp_mailonline/paragraphs/1_2_2_countchange_test.clj
@@ -1,3 +1,7 @@
 (ns sicp-mailonline.paragraphs.1-2-2-countchange-test
   (:require [midje.sweet :refer :all]
             [sicp-mailonline.paragraphs.1-2-2-countchange :refer :all]))
+
+(facts "dollar change"
+       (fact "recursive implementation"
+             (count-change 100) => 292))
