40655b8475cb20318a2d0647584e4d4c3673bfa3
reborg <reborg@reborg.net>
2015-01-06 06:57:19 +0000

    Should not leave main recursion to iterate over odd

--- src/sicp_mailonline/exercises/1_16.clj
+++ src/sicp_mailonline/exercises/1_16.clj
@@ -2,10 +2,10 @@
   (:require [sicp-mailonline.paragraphs.1-2-4 :as expt]))
 
 (defn iter-fast-expt [b n]
-  (if (even? n)
-    (if (zero? n) 1
-      (loop [acc b counter n]
-        (if (= 1 counter)
-          acc
-          (recur (expt/square acc) (/ counter 2)))))
-    (expt/i-expt b n)))
+  (if (zero? n) 1
+    (loop [acc b counter n]
+      (if (= 1 counter)
+        acc
+        (if (even? n)
+          (recur (expt/square acc) (/ counter 2))
+          (recur (* acc b) (dec counter)))))))
