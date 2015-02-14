b9877909683f7ec90eeb219454eab35d41a2668f
Nigel Perkins <nigelhp@yahoo.co.uk>
2015-02-14 17:54:38 +0000

    Further examples from 2.2.1

--- src/sicp_mailonline/examples/2_2_1.clj
+++ src/sicp_mailonline/examples/2_2_1.clj
@@ -10,3 +10,18 @@
 
 (defn scale-list [items factor]
   (map (fn [x] (* x factor)) items))
+
+(defn list-ref [items n]
+  (if (zero? n)
+    (first items)
+    (recur (rest items) (dec n))))
+
+(defn length [items]
+  (if (empty? items)
+    0
+    (+ 1 (length (rest items)))))
+
+(defn append [list1 list2]
+  (if (empty? list1)
+    list2
+    (cons (first list1) (append (rest list1) list2))))
--- test/sicp_mailonline/examples/2_2_1_test.clj
+++ test/sicp_mailonline/examples/2_2_1_test.clj
@@ -4,15 +4,36 @@
   (:require [sicp-mailonline.examples.2-2-1 :refer :all]
             [clojure.test :refer :all]))
 
+(def ^:private ^:const squares (list 1 4 9 16 25))
+(def ^:private ^:const odds (list 1 3 5 7))
+
 (deftest example2-2-1
   (testing "map abs"
     (is (= '(10 2.5 11.6 17)
            (map #(Math/abs %) (list -10 2.5 -11.6 17)))))
 
   (testing "map square"
-    (is (= '(1 4 9 16)
-           (map #(* % %) (list 1 2 3 4)))))
+    (is (= squares
+           (map #(* % %) (list 1 2 3 4 5)))))
 
   (testing "scale-list"
     (is (= '(10 20 30 40 50)
-           (scale-list (list 1 2 3 4 5) 10)))))
+           (scale-list (list 1 2 3 4 5) 10))))
+
+  (testing "list-ref"
+    (is (= 16
+           (list-ref squares 3))))
+  
+  (testing "length"
+    (is (= 4
+           (length odds)))
+
+    (is (= 0
+           (length '()))))
+
+  (testing "append"
+    (is (= '(1 4 9 16 25 1 3 5 7))
+        (append squares odds))
+
+    (is (= '(1 3 5 7 1 4 9 16 25))
+        (append odds squares))))
