fdaf26722babe827e432ae06e51099f4ab5620b0
Nigel Perkins <nigelhp@yahoo.co.uk>
2015-04-03 20:26:42 +0100

    Scaling examples from 2.2.2

--- src/sicp_mailonline/examples/2_2_2.clj
+++ src/sicp_mailonline/examples/2_2_2.clj
@@ -7,3 +7,16 @@
         (empty? x) 0
         :else (+ (count-leaves (first x))
                  (count-leaves (rest x)))))
+
+(defn recursive-scale-tree [tree factor]
+  (cond (not (coll? tree)) (* tree factor)
+        (empty? tree) '()
+        :else (cons (recursive-scale-tree (first tree) factor)
+                    (recursive-scale-tree (rest tree) factor))))
+
+(defn map-scale-tree [tree factor]
+  (map (fn [sub-tree]
+         (if (coll? sub-tree)
+           (map-scale-tree sub-tree factor)
+           (* sub-tree factor)))
+       tree))
--- test/sicp_mailonline/examples/2_2_2_test.clj
+++ test/sicp_mailonline/examples/2_2_2_test.clj
@@ -5,6 +5,7 @@
             [clojure.test :refer :all]))
 
 (def ^:private ^:const x (cons (list 1 2) (list 3 4)))
+(def ^:private ^:const tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
 
 (deftest example2-2-2
   (testing "count-leaves"
@@ -18,4 +19,12 @@
            (count-leaves '())))
 
     (is (= 1
-           (count-leaves (list 7))))))
+           (count-leaves (list 7)))))
+  
+  (testing "recursive scale-tree"
+    (is (= '(10 (20 (30 40) 50) (60 70))
+           (recursive-scale-tree tree 10))))
+
+  (testing "map scale-tree"
+    (is (= '(10 (20 (30 40) 50) (60 70))
+           (map-scale-tree tree 10)))))
