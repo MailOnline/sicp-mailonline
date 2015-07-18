ac803d2d6ca9d2f97d250bcfcf199dd619cac181
Nigel Perkins <nigelhp@yahoo.co.uk>
2015-07-18 21:51:26 +0100

    Upgrade cider version

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :min-lein-version "2.0.0"
-  :profiles {:dev {:plugins [[cider/cider-nrepl "0.8.2"]]}})
+  :profiles {:dev {:plugins [[cider/cider-nrepl "0.9.1"]]}})
