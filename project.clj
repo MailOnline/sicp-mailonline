04c61f298a41233da54b2064b9d5e770dfc6b751
Nigel Perkins <nigelhp@yahoo.co.uk>
2015-08-22 16:42:50 +0100

    Upgrade to Clojure 1.7

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
 (defproject sicp-mailonline "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :description "SICP in Clojure"
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :min-lein-version "2.0.0"
   :profiles {:dev {:plugins [[cider/cider-nrepl "0.9.1"]]}})
