0684376b3eab3519487f4342e673499a7913a57a
Nigel Perkins <nigelhp@yahoo.co.uk>
2015-09-20 21:36:28 +0100

    Correct typo in question hint

--- src/sicp_mailonline/exercises/3_17.scm
+++ src/sicp_mailonline/exercises/3_17.scm
@@ -1,6 +1,6 @@
 ;; Exercise 3.17
 ;; Devise a correct version of the count-pairs procedure of exercise 3.16 that
-;; returns the number of distinct pairs in any structure.  (Hint"" Traverse the
+;; returns the number of distinct pairs in any structure.  (Hint: Traverse the
 ;; structure, maintaining an auxiliary data structure that is used to keep track
 ;; of which pairs have already been counted.)
 
